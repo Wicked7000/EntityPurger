3591dcd23f02d2901ac100798f18ddb5b786d5dc
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-05-13 17:49:26 +0100

    Should always follow order of entitySettings even if a direct match

--- src/main/java/com/wicked/entitypurger/configuration/ConfigManager.java
+++ src/main/java/com/wicked/entitypurger/configuration/ConfigManager.java
@@ -217,17 +217,12 @@ public class ConfigManager {
     }
 
     public EntitySettings getSettingsForEntity(String entityId) {
-        EntitySettings settings = entitySettings.getOrDefault(entityId, null);
-        if(Objects.isNull(settings)){
-            for(String entityKey : entitySettings.keySet()){
-                Pattern pattern = Pattern.compile(entityKey);
-                Matcher matcher = pattern.matcher(entityId);
-                if(matcher.matches()){
-                    return entitySettings.get(entityKey);
-                }
+        for(String entityKey : entitySettings.keySet()){
+            Pattern pattern = Pattern.compile(entityKey);
+            Matcher matcher = pattern.matcher(entityId);
+            if(matcher.matches()){
+                return entitySettings.get(entityKey);
             }
-        }else{
-            return settings;
         }
 
         return defaultEntitySettings;
